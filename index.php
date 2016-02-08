e245227b27441e59ee84c8014ecfc897d6f005c1
GuillaumeCa <guillaume.carre@isep.fr>
2016-02-08 10:10:29 +0100

    initialisation

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil - Motiv'Mate</title>
+  </head>
+  <body>
+    <h1>Bienvenue sur Motiv'Mate !</h1>
+  </body>
+</html>
