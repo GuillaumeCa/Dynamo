3ac3401b2af9a8034156a808898cf0be6323e706
GuillaumeCa <guillaume.carre@isep.fr>
2016-02-14 19:41:18 +0100

    test layout fenêtre recherche

--- connect.php
+++ connect.php
@@ -15,7 +15,59 @@
       <div class="search">
         <input type="text" name="name" placeholder="rechercher">
         <div class="result">
-
+          <div class="cat">
+            <div class="head">
+              <span class="title">GROUPES</span>
+              <a href="#">VOIR</a>
+            </div>
+            <ul>
+              <li>
+                <div class="image" style="background-image: url('assets/images/logo.png')"></div>
+                <div class="text">
+                  <h2>Nom groupe</h2>
+                  <h3><b>Sport</b> basketball</h3>
+                  <h3><b>Lieu</b> Paris</h3>
+                </div>
+                <span>2<span class="small">/7</span></span>
+              </li>
+              <li>
+                <div class="image" style="background-image: url('assets/images/logo.png')"></div>
+                <div class="text">
+                  <h2>Nom groupe</h2>
+                  <h3><b>Sport</b> basketball</h3>
+                  <h3><b>Lieu</b> Paris</h3>
+                </div>
+                <span>1<span class="small">/7</span></span>
+              </li>
+            </ul>
+          </div>
+          <div class="cat">
+            <div class="head">
+              <span class="title">GROUPES</span>
+              <a href="#">VOIR</a>
+            </div>
+            <ul>
+              <a href="#">
+              <li>
+                <div class="image" style="background-image: url('assets/images/logo.png')"></div>
+                <div class="text">
+                  <h2>Nom groupe</h2>
+                  <h3><b>Sport</b> basketball</h3>
+                  <h3><b>Lieu</b> Paris</h3>
+                </div>
+                <span>2<span class="small">/7</span></span>
+              </li></a>
+              <li>
+                <div class="image" style="background-image: url('assets/images/logo.png')"></div>
+                <div class="text">
+                  <h2>Nom groupe</h2>
+                  <h3><b>Sport</b> basketball</h3>
+                  <h3><b>Lieu</b> Paris</h3>
+                </div>
+                <span>1<span class="small">/7</span></span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <nav>
