888fb92b01e0f190022504791961f759d4cc63cd
Pod4994 <pierreolivier.dezard@gmail.com>
2016-02-29 09:34:13 +0100

    création page inscription

---
+++ inscription.php
@@ -0,0 +1,15 @@
+<?php
+include 'function.php';
+getLanguage('fr');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
+    <title>Inscription</title>
+  </head>
+  <body>
+    <?php include 'assets/template/header.php' ?>
+  </body>
+</html>
