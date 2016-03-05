daab982b4fa469da09978a880d0032bda45d863d
GuillaumeCa <guillaume.carre@isep.fr>
2016-03-05 23:44:52 +0100

    ajout page accueil privé

---
+++ assets/template/header-priv.php
@@ -0,0 +1,75 @@
+<header>
+  <div class="logo">
+    <a href="index.php">
+    <img src="assets/images/logo.png" alt="logo" />
+    </a>
+  </div>
+  <div class="search">
+    <input type="text" name="name" placeholder="rechercher">
+    <div class="result">
+      <div class="cat">
+        <div class="head">
+          <span class="title">GROUPES</span>
+          <a href="#">VOIR</a>
+        </div>
+        <ul>
+          <li>
+            <div class="image" style="background-image: url(/assets/images/sport1.png)"></div>
+            <div class="text">
+              <h2>Nom groupe</h2>
+              <h3><b>Sport</b> basketball</h3>
+              <h3><b>Lieu</b> Paris</h3>
+            </div>
+            <span>2<span class="small">/7</span></span>
+          </li>
+          <li>
+            <div class="image" style="background-image: url(/assets/images/sport1.png)"></div>
+            <div class="text">
+              <h2>Nom groupe</h2>
+              <h3><b>Sport</b> basketball</h3>
+              <h3><b>Lieu</b> Paris</h3>
+            </div>
+            <span>1<span class="small">/7</span></span>
+          </li>
+        </ul>
+      </div>
+      <div class="cat">
+        <div class="head">
+          <span class="title">GROUPES</span>
+          <a href="#">VOIR</a>
+        </div>
+        <ul>
+          <li>
+            <div class="image" style="background-image: url(/assets/images/sport1.png)"></div>
+            <div class="text">
+              <h2>Nom groupe</h2>
+              <h3><b>Sport</b> basketball</h3>
+              <h3><b>Lieu</b> Paris</h3>
+            </div>
+            <span>2<span class="small">/7</span></span>
+          </li>
+          <li>
+            <div class="image" style="background-image: url(/assets/images/sport1.png)"></div>
+            <div class="text">
+              <h2>Nom groupe</h2>
+              <h3><b>Sport</b> basketball</h3>
+              <h3><b>Lieu</b> Paris</h3>
+            </div>
+            <span>1<span class="small">/7</span></span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <a href="" class="profile-btn">
+    D
+  </a>
+  <nav>
+    <ul>
+      <li><a href="connect.php"><?php lang('Groupes') ?> <span class="notif">3</span></a></li>
+      <li><a href="#"><?php lang('Forum') ?> <span class="notif">30</span></a></li>
+      <li><a href="help.php"><?php lang('Aide') ?></a></li>
+    </ul>
+  </nav>
+</header>
