21aec3ca547cc2e90c96d46079298fa57c535a57
Djole <db_srb@hotmail.fr>
2016-03-11 18:38:38 +0100

    test de push

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php
+include 'function.php';
+getLanguage('fr');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
+    <title><?php lang('Aide') ?> - Dynamo</title>
+  </head>
+  <body>
+    <?php include 'assets/template/header.php'; ?>
+
+    <section class="pink-bg list">
+      <div>
+        <article>
+          <h1>Comment se connecter&nbsp;?</h1>
+          <p>
+            Rendez vous sur la page <a href="connect.php">Connexion</a>.
+          </p>
+        </article>
+      </div>
+    </section>
+
+    <?php include 'assets/template/footer.php'; ?>
+  </body>
+</html>
