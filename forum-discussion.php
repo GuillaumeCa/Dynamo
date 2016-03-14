b83e8079f3b43ec4b4a4e329e534bd4f5627c41c
CelesteBegassat <celeste.ambre@hotmail.fr>
2016-03-14 14:21:17 +0100

    Début page Forum

---
+++ forum-discussion.php
@@ -0,0 +1,24 @@
+<?php
+include 'function.php';
+getLanguage('fr');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
+    <title>Dynamo</title>
+  </head>
+  <body>
+    <?php include 'assets/template/header-priv.php' ?>
+    <?php require 'assets/images/svg.php' ?>
+    <section class="pink-bg">
+      <div class="column">
+        <h1>Forum</h1>
+        <p>Une question ? Une idée ? Votre espace de discussion publique totalement dédié au sport !</p>
+      </div>
+    </section>
+    
+    <?php include 'assets/template/footer.php'; ?>
+  </body>
+</html>
