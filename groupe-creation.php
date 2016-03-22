0ed2384d9ef8a8f311b3aa76113fddf5964a1987
GuillaumeCa <guillaume.carre@isep.fr>
2016-03-22 10:31:28 +0100

    reformatage css formulaire

--- groupe-creation.php
+++ groupe-creation.php
@@ -19,25 +19,25 @@ getLanguage('fr');
     <section>
       <div class="column">
         <form class="groupe_crea" action="index.php" method="post">
-          <h2 class="pink-text">Nom de groupe</h2>
+          <h2 class="form-label pink-text">Nom de groupe</h2>
           <input class="clear-form" type="text" name="name_grp" placeholder="">
-          <h2 class="pink-text">Ajouter vos amis</h2>
+          <h2 class="form-label pink-text">Ajouter vos amis</h2>
           <input class="clear-form" type="text" name="membre" placeholder="Rentrer les pseudos ou les e-mails">
-          <h2 class="pink-text">Votre sport</h2>
+          <h2 class="form-label pink-text">Votre sport</h2>
           <input class="clear-form" type="text" name="sport" placeholder="Football, Athlétisme, Rugby ...">
-          <h2 class="pink-text">Club</h2>
+          <h2 class="form-label pink-text">Club</h2>
           <input class="clear-form" type="text" name="club" placeholder="Stade Francais, Athlé 91 ...">
-          <h2 class="pink-text">Lieu</h2>
+          <h2 class="form-label pink-text">Lieu</h2>
           <input class="clear-form" type="text" name="lieu" placeholder="Paris, 75015, Essonne ...">
-          <h2 class="pink-text">Combien de membres ?</h2>
-          <select class="clear-form" name="jour">
+          <h2 class="form-label pink-text">Combien de membres ?</h2>
+          <select class="clear-form dropdown dropdown-lg" name="jour">
             <option value="option" disabled selected>Membres</option>
             <?php for ($i = 0; $i < 7; $i++): ?>
               <option value="<?php echo $i ?>"><?php echo $i+1; ?></option>
             <?php endfor; ?>
           </select>
-          <h2 class="pink-text">Descritpion du groupe</h2>
-          <input class="clear-form" type="text" name="description_grp" placeholder="Décrivez votre groupe en quelques lignes ...">
+          <h2 class="form-label pink-text">Descritpion du groupe</h2>
+          <textarea class="clear-form" name="description_grp" rows="6" cols="40" placeholder="Décrivez votre groupe en quelques lignes ..."></textarea>
       </div>
     </section>
   </body>
