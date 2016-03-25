59f9d43d505d75256e7494936ba0964aaf7b75a5
CelesteBegassat <celeste.ambre@hotmail.fr>
2016-03-25 11:48:12 +0100

    forum discussion début

--- forum-discussion.php
+++ forum-discussion.php
@@ -12,13 +12,14 @@ getLanguage('fr');
   <body>
     <?php include 'assets/template/header-priv.php' ?>
     <?php require 'assets/images/svg.php' ?>
-    <section class="pink-bg">
-      <div class="column">
-        <h1>Forum</h1>
-        <p>Une question ? Une idée ? Votre espace de discussion publique totalement dédié au sport !</p>
+    <section class="info-groupe" style="background-image: url(/assets/images/sport1.png);">
+      <div class="overlay"></div>
+      <div class="en-tete-groupe">
+        <h1>Discussion #1</h1>
+        <p>Créée le 22/03/2016 par Celeste Bégassat</p>
       </div>
     </section>
-    
+
     <?php include 'assets/template/footer.php'; ?>
   </body>
 </html>
