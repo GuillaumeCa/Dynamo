c125211ff1459d16b1e9787d25ec987e99612e1f
AlizeeFaytre <afaytre@juniorisep.com>
2016-05-20 11:52:36 +0200

    dynamisme de l'header des pages groupe

--- view/template/groupe-header.php
+++ view/template/groupe-header.php
@@ -7,11 +7,31 @@
   </div>
   <div class="sec-overlay sec-over-violet"></div>
   <div class="auto-width group">
-    <h1 class="ttl-md"><?php echo $presentation_groupe->nomGroupe ?></h1>
-    <p class="txt-jdesc"><?php echo $presentation_groupe->description ?> </p>
-    <div class="txt-info">
-      <span><b>Sport</b> <?php echo $presentation_groupe->sport ?></span>
-      <span><b>Club</b> <?php echo $presentation_groupe->club ?></span>
-    </div>
+    <form class="modifierinfo" action="" method="post">
+      <h1 class="ttl-md">
+        <span class="header_groupe"><?php echo $presentation_groupe->nomGroupe ?></span>
+        <input class="modif-form" type="text" name="name" value="<?php echo $presentation_groupe->nomGroupe ?>">
+      </h1>
+      <p class="txt-jdesc">
+        <span class="header_groupe"><?php echo $presentation_groupe->description ?></span>
+        <input class="modif-form" type="text" name="name" value="<?php echo $presentation_groupe->description ?>">
+      </p>
+      <div class="txt-info">
+        <span>
+          <b>Sport</b>
+          <span class="header_groupe"><?php echo $presentation_groupe->sport ?> </span>
+          <input class="modif-form" type="text" name="name" value="<?php echo $presentation_groupe->sport ?>">
+        </span>
+        <span>
+          <b>Club</b>
+          <span class="header_groupe"> <?php echo $presentation_groupe->club ?></span>
+          <input class="modif-form" type="text" name="name" value="<?php echo $presentation_groupe->club ?>">
+        </span>
+      </div>
+    </form>
+    <?php var_dump($isLeader) ?>
+    <?php if ($isLeader): ?>
+      <a href="#" class="button btn-sm btn-right btn-wh-inv" id="InfoModifier">modifier</a>
+    <?php endif; ?>
   </div>
 </section>
